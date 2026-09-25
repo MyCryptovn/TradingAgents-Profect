34ea0b7a86f7ab6124fa5500fd3e45843d2f66a7
crytopool <bhuong832017@gmail.com>
2026-09-25 12:31:01 +0700

    Increase TOP_N and PREFILTER_N values

--- scripts/market_scanner.py
+++ scripts/market_scanner.py
@@ -15,8 +15,8 @@ import urllib.request
 from dataclasses import dataclass
 
 API = "https://api.kraken.com/0/public/"
-TOP_N = 1
-PREFILTER_N = 5
+TOP_N = 3
+PREFILTER_N = 10
 MIN_VOLUME_USD = 1_000_000.0
 TIMEFRAMES = (15, 60, 240)
 
